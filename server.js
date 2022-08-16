dbd77d8eceec53bd176c3e2d2d6d02f9244fdd5a
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-08-15 20:32:32 -0500

    Mern-Project back-end - cors - END

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const userRoutes = require("./routes/user.routes");
-const postRoutes = require("./routes/post.routes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
 
-require("dotenv").config({ path: "./config/.env" });
-require("./config/db");
-const { checkUser, requireAuth } = require("./middleware/auth.middleware");
-const cors = require("cors");
+require('dotenv').config({ path: './config/.env' });
+require('./config/db');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
+const cors = require('cors');
 
 const app = express();
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
-  allowedHeaders: ["sessionId", "Content-Type"],
-  exposedHeaders: ["sessionId"],
-  methods: "GET, HEAD, PUTCH, PATCH, POST, DELETE",
-  prefLightContinue: false,
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'methods': 'GET, HEAD, PUTCH, PATCH, POST, DELETE',
+  'preflightContinue': false
 };
 app.use(cors(corsOptions));
 
@@ -26,14 +26,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // jwt
-app.get("*", checkUser);
-app.get("/jwtid", requireAuth, (req, res) => {
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
   res.status(200).send(res.locals.user._id);
 });
 
 // routes
-app.use("/api/user", userRoutes);
-app.use("/api/post", postRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 // server
 app.listen(process.env.PORT, () => {
